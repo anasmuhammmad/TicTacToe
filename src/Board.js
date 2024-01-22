7a6f3a005135d47d49f3916dc82c127ad020db15
Anas <anasmuhammed444@gmail.com>
2024-01-22 18:17:33 +0530

    Initial Commit - background colour -basic design

---
+++ src/Board.js
@@ -0,0 +1,22 @@
+// import { View, Text, TouchableOpacity, Text, StyleSheet} from 'react-native'
+// import React from 'react'
+
+// const Board = ({board,onPress}) => {
+//   return (
+//     <View style{styles.board}>
+//       {board.map((row, rowIndex) =>( 
+//       <View key={rowIndex} style={styles.row}>
+//       {
+//         row.map((cell, cellIndex) => (
+//             <TouchableOpacity key={cell}>
+
+//             </TouchableOpacity>
+//         ))
+//       }
+//       </View>
+//       ))}
+//     </View>
+//   )                             
+// }
+
+// export default Board
---
+++ src/Game.js
@@ -0,0 +1,21 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+const Game = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Gamedf</Text>
+    </View>
+  );
+};
+
+export default Game;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
